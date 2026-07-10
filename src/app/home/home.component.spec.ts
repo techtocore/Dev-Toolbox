d4c2c6b7bddc692faae652c2c919545f98e99597
techtocore <akashkravi@gmail.com>
2026-07-09 18:48:20 -0700

    bugfix: add mousedown event handling to search results for improved navigation

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
@@ -26,5 +27,23 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should keep a search result available through mousedown and navigate on click', () => {
+    const router = TestBed.inject(Router);
+    const navigateSpy = spyOn(router, 'navigate').and.resolveTo(true);
+
+    component.searchQuery = 'base64';
+    component.onSearchChange();
+    fixture.detectChanges();
+
+    const result = fixture.nativeElement.querySelector('.search-result-item') as HTMLElement;
+    const mouseDown = new MouseEvent('mousedown', { bubbles: true, cancelable: true });
+
+    result.dispatchEvent(mouseDown);
+    expect(mouseDown.defaultPrevented).toBeTrue();
+
+    result.click();
+    expect(navigateSpy).toHaveBeenCalledOnceWith(['/base64']);
   });
 });
